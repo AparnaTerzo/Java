7fc4b57980f7d38f22d60acaa81d2b399b611ca8
AparnaTerzo <aparna@terzocloud.com>
2023-06-13 15:42:43 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import java.text.NumberFormat;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        String currency=NumberFormat.getCurrencyInstance().format(789450362.98);
+        System.out.println(currency);
 
+        //percentage
+        String percent = NumberFormat.getPercentInstance().format(0.099);
+        System.out.println(percent);
     }
 }
