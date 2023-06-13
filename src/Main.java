8129b2292e7b2ffd8b34740ee9a852398796e562
AparnaTerzo <aparna@terzocloud.com>
2023-06-13 11:11:00 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,13 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        //Single Variable
+        int number = 50;
+        System.out.println("The Number : "+number);
 
+        //Assigning value for a variable
+        int age = 20;
+        int newAge = age;
+        System.out.println("New Age is " +newAge);
     }
 }
