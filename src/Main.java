018f64710b5fc4bd9e52cd8da86b1f1fc23a70ca
AparnaTerzo <aparna@terzocloud.com>
2023-06-13 15:27:56 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,33 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        // Round
+        int round = Math.round(2.8f);
+        System.out.println("Rounded value = "+round);
+
+        //ceil
+        int ceil = (int)Math.ceil(2.2f);
+        System.out.println("Ceiled value = "+ceil);
+
+        //floor
+        int floor = (int)Math.floor(2.8f);
+        System.out.println("Floor value = "+floor);
+
+        //max
+        int max = Math.max(9,78);
+        System.out.println("Max value = "+max);
+
+        //min
+        int min = Math.min(9,78);
+        System.out.println("Min value = "+min);
+
+        //max
+        int random = (int) Math.round(Math.random()*100);
+        System.out.println("Random value = "+random);
+
+
+
+
 
     }
 }
