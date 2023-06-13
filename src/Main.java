01a8805594fd454a1350dc6ca3accbcfe03e6c5b
AparnaTerzo <aparna@terzocloud.com>
2023-06-13 11:36:06 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,15 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        byte age = 20;
+        char letter = 'C';
+        boolean isEligibleForPlacement = true;
+        float cgpa = 8.64f;
+        long sap = 92;
+
+        System.out.println("Age : " +age);
+        System.out.println("CGPA : " +cgpa);
+        System.out.println("Sap Points : " +sap);
 
     }
 }
