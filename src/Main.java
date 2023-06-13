4d747f3fe07de583ea955c887ae37a3ef2f08382
AparnaTerzo <aparna@terzocloud.com>
2023-06-13 12:46:18 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
-
+        System.out.println("Hi \"Everyone\"");
+        System.out.println("c:\\windows\\..");
+        System.out.println("d:\nwindows..");
+        System.out.println("c:\twindows..");
+        // \n => new line
+        // \t => tab space
     }
 }
