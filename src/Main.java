bcf22f9e32c2620bb2b88a24849404d930fc63ea
AparnaTerzo <aparna@terzocloud.com>
2023-06-13 22:06:38 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -7,25 +7,43 @@ public class Main {
     public static void main(String[] args) {
         final int month = 12;
         final int percent = 100;
+        int principal =0;
+        float monthlyRate = 0;
+        int numberOfPayments=0;
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the principal : ");
-        int principal = scanner.nextInt();
+        while (true) {
+            System.out.print("Enter the principal : ");
+             principal = scanner.nextInt();
+            if (principal >= 1000 && principal <= 1000000)
+                break;
+            System.out.println("Enter the value between 1000 and 10000000");
+        }
+        while(true) {
+            System.out.print("Enter the value of rate : ");
+            float annualRate = scanner.nextFloat();
+            if(annualRate >=1 && annualRate <=30) {
+                monthlyRate = annualRate / percent / month;
+                break;
+            }
+            System.out.println("Enter the value greater than 0 and less than or equal to 30");
+        }
+        while(true) {
+            System.out.print("Enter the number of years : ");
+            int year = scanner.nextInt();
+            if (year >= 1 && year <= 30) {
+                 numberOfPayments = year * month;
+                break;
+            }
+            System.out.println("Enter the value between 1 and 30");
+        }
 
-        System.out.print("Enter the value of rate : ");
-        float annualRate = scanner.nextFloat();
-        float monthlyRate = annualRate /percent/month;
+            double mortage = principal *
+                    (monthlyRate * Math.pow(1 + monthlyRate, numberOfPayments)) /
+                    (Math.pow(1 + monthlyRate, numberOfPayments) - 1);
 
-
-        System.out.print("Enter the number of years : ");
-        int year = scanner.nextInt();
-        int numberOfPayments = year * month;
-
-        double mortage = principal *
-                (monthlyRate * Math.pow(1 + monthlyRate,numberOfPayments))/
-                        (Math.pow(1 +monthlyRate,numberOfPayments) -1);
-
-        String mortageFormat = NumberFormat.getCurrencyInstance().format(mortage);
-        System.out.println("Mortage : " +mortageFormat);
+            String mortageFormat = NumberFormat.getCurrencyInstance().format(mortage);
+            System.out.println("Mortage : " + mortageFormat);
+        }
     }
-}
