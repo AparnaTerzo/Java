3d3daf8ffccbd973ef2f0a43bfb077bb4b0e2f18
AparnaTerzo <aparna@terzocloud.com>
2023-06-13 13:01:18 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
+import java.util.Arrays;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int[] numbers = new int[5];
+        numbers[0]=10;
+        numbers[2]=30;
+        System.out.println(Arrays.toString(numbers));
 
+        //Another way to initialize in an array
+        int[] number ={10,5,20,15,40};
+        System.out.println(number.length);
+
+        //sorting a array
+        Arrays.sort(number);
+        System.out.println(Arrays.toString(number));
     }
 }
