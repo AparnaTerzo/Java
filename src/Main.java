fb4dbb9af505d8685c3e0f3e574d806c850513e0
AparnaTerzo <aparna@terzocloud.com>
2023-06-13 16:07:51 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Scanner scanner = new Scanner (System.in);
 
+        // For String
+        System.out.print("Enter Your Firstname : ");
+        String firstName = scanner.nextLine();
+        System.out.print("Enter Your Lastname : ");
+        String lastName = scanner.nextLine();
+        String name = firstName +lastName;
+        System.out.println("Name is " +name);
+        System.out.println("Name is " +(name.trim()));
+
+        // Integer
+        System.out.print("Enter your age : ");
+        int age = scanner.nextInt();
+        System.out.println("Age is " +age);
     }
 }
