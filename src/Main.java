9097aaf88e4db7f85a5cd2818b088e5a327f1e6a
AparnaTerzo <aparna@terzocloud.com>
2023-06-13 12:02:05 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import java.awt.*;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
-
+        Point point1 = new Point(1,1);
+        Point point2 = point1;
+        point1.y=10;
+        System.out.println(point1);
+        System.out.println(point2);
     }
 }
